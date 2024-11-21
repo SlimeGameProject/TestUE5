c7d1648a66bd3a2c7cffab01adedcec0c916fa0d
Anoho <anohoss.programmer@gmail.com>
2024-11-22 00:41:14 +0900

    update SlimeCharacter
    追加
    - スライムの変態処理を SlimeCharacterSubsystem へ移行しました

---
+++ FallingSlime/Source/FallingSlime/Private/Slime/Character/FallingSlimeSlimeCharacterSubsystem.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Slime/Character/FallingSlimeSlimeCharacterSubsystem.h"
+
+// Include headers in Engine module.
+#include "Components/CapsuleComponent.h"
+
+AFallingSlimeSlimeCharacter* UFallingSlimeSlimeCharacterMetamorphosisSubsystem::MetamorphoseSlimeCharacter(AFallingSlimeSlimeCharacter* OriginalSlimeCharacter, const UFallingSlimeSlimeCharacterMetamorphosisData* MetamorphosisData)
+{
+	if (!OriginalSlimeCharacter)
+	{
+		return nullptr;
+	}
+
+	if (!MetamorphosisData)
+	{
+		return nullptr;
+	}
+
+	AFallingSlimeSlimeCharacter* MetamorphosedSlimeCharacter = nullptr;
+
+	if (UWorld* World = OriginalSlimeCharacter->GetWorld())
+	{
+		UCapsuleComponent* OriginalCapsuleComp = OriginalSlimeCharacter->GetCapsuleComponent();
+		// 衝突して新しいスライムのスポーン位置がずれてしまうため、コリジョンを無効化する
+		ECollisionEnabled::Type OriginalCollisionEnabled = OriginalCapsuleComp->GetCollisionEnabled();
+		OriginalCapsuleComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+		TSubclassOf<AFallingSlimeSlimeCharacter> SlimeCharacterClassToMetamorphose = MetamorphosisData->GetSlimeCharacterClassToMetamorphose();
+		FActorSpawnParameters ActorSpawnParams;
+		FTransform ActorSpawnTransform = OriginalSlimeCharacter->GetActorTransform();
+
+		MetamorphosedSlimeCharacter = World->SpawnActor<AFallingSlimeSlimeCharacter>(SlimeCharacterClassToMetamorphose, ActorSpawnTransform, ActorSpawnParams);
+		MetamorphosedSlimeCharacter->GetCapsuleComponent()->SetCollisionEnabled(OriginalCollisionEnabled);
+
+		AController* OriginalController = OriginalSlimeCharacter->Controller;
+		const FRotator OriginalControlRotation = OriginalSlimeCharacter->GetControlRotation();
+
+		OriginalController->Possess(MetamorphosedSlimeCharacter);
+		// 所有時にアクタの正面に ControlRotation を合わせてしまうため、修正する
+		OriginalController->SetControlRotation(OriginalControlRotation);
+
+		MetamorphosedSlimeCharacter->MetamorphosedFrom(OriginalSlimeCharacter);
+
+		OriginalSlimeCharacter->Destroy();
+	}
+
+	return MetamorphosedSlimeCharacter;
+}
